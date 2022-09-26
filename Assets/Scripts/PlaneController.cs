d0ceae0e2e1843e022f2e7fb274e3b2bdbd487b7
roman garms <romangarms@gmail.com>
2022-09-26 09:41:20 -0700

    plane controller

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float rotationSpeedLR;
+    public float verticalInput;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical/horizontal input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+
+        // tilt the plane left/right based on left/right arrow keys
+        transform.Rotate(Vector3.forward * rotationSpeedLR * Time.deltaTime * horizontalInput * -1);
+    }
+}
