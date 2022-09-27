7617c37859b6b53f11e8235a378f5ac7f8513643
roman garms <romangarms@gmail.com>
2022-09-27 09:21:57 -0700

    Basic Flight Controller

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,7 +15,7 @@ public class CameraFollow : MonoBehaviour {
 	{
 		Vector3 desiredPosition = target.position + offset;
 		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-		transform.position = smoothedPosition;
+		transform.position = desiredPosition;
 
 		transform.LookAt(target);
     
--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -5,31 +5,56 @@ using UnityEngine;
 public class PlaneController : MonoBehaviour
 {
     public float speed = 10;
+
     public float rotationSpeed = 10;
+
     public float rotationSpeedLR = 10;
-    public float verticalInput = 10;
-    public float horizontalInput = 10;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float xReset = 50;
 
-    }
+    public bool invertedVerticalControl = false;
+
+    private float verticalInput;
+
+    private float horizontalInput;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (GetComponent<Transform>().position.x > xReset)
+        {
+            transform.position =
+                new Vector3(0, transform.position.y, transform.position.z);
+        }
+
         // get the user's vertical/horizontal input
-        verticalInput = Input.GetAxis("Vertical");
+        if (invertedVerticalControl == true)
+        {
+            verticalInput = -1 * (Input.GetAxis("Vertical"));
+        }
+        else
+        {
+            verticalInput = (Input.GetAxis("Vertical"));
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate (model is rotated right, so moving left cancels out)
         transform.Translate(Vector3.left * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime * verticalInput);
+        transform
+            .Rotate(Vector3.forward *
+            rotationSpeed *
+            Time.deltaTime *
+            verticalInput *
+            -1);
 
         // tilt the plane left/right based on left/right arrow keys
-        transform.Rotate(Vector3.right * rotationSpeedLR * Time.deltaTime * horizontalInput);
+        transform
+            .Rotate(Vector3.right *
+            rotationSpeedLR *
+            Time.deltaTime *
+            horizontalInput);
     }
 }
